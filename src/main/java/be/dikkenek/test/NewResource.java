1fa32e58c2426c23da981456de4bf9fd5ab36d2b
NextSap <nextsap@outlook.com>
2026-09-22 17:49:00 +0200

    modify new resource test

--- src/main/java/be/dikkenek/test/NewResource.java
+++ src/main/java/be/dikkenek/test/NewResource.java
@@ -4,11 +4,11 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
-@Path("/hello-world")
+@Path("/new-resource")
 public class NewResource {
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello, World!";
+        return "New, resource!";
     }
 }
